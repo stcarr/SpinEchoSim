42ad0fc0e780aadfe5ce9c5ed8bd90953a6243f1
charlessnider <charles_snider@brown.edu>
2020-06-23 15:18:41 -0400

    Added spatial distribution, restructured parameter files into dictionaries, ran some tests

--- SpinSimParams.jl
+++ SpinSimParams.jl
@@ -1,125 +1,129 @@
 module SpinSimParams
 
     using StaticArrays
-    using StatsBase # for sample()
+    using StatsBase
 
+    function make_params(γ, τ, dt, α, ω, ν0, bw, nfreq, ρ0)
 
-    mutable struct make_parameters
+        f = Dict();
 
-        # operators
-        M_op
-        Iz
-        UL90
-        UR90
+        # magnetization
+        f["Ix"] = @SMatrix [0 1/2; 1/2 0];
+        f["Iy"] = @SMatrix [0 -1im/2; 1im/2 0];
+        f["Iz"] = @SMatrix [1/2 0; 0 -1/2];
+        f["M_op"] = f["Ix"] + 1im*f["Iy"];
+
+        # pulse operators
+        f["UL90"] = exp(-1im*pi*f["Ix"]/2);
+        f["UR90"] = exp(1im*pi*f["Ix"]/2);
 
         # time variables
-        γ
-        τ
-        dt
+        f["γ"] = γ;
+        f["τ"] = τ;
+        f["dt"] = dt;
 
         # interaction variables
-        α
-        ω
+        f["α"] = α;
+        f["ω"] = ω;
 
         # spin ensemble variables
-        ν0
-        bw
-        dfreq
-        ρ0
-        r # positions of spins
+        f["ν0"] = ν0;
+        f["bw"] = bw;
+        f["nfreq"] = nfreq;
+        f["ρ0"] = ρ0;
 
-        # dissipation parameters
-        Lj # collection of jump operators
-
-        function make_parameters(γ, τ, dt, α, ω, ν0, bw, dfreq, ρ0)
-
-            # magnetization
-            Ix = @SMatrix [0 1/2; 1/2 0];
-            Iy = @SMatrix [0 -1im/2; 1im/2 0];
-            Iz = @SMatrix [1/2 0; 0 -1/2];
-            M_op = Ix + 1im*Iy;
-
-            # pulse operators
-            UL90 = exp(-1im*pi*Ix/2);
-            UR90 = exp(1im*pi*Ix/2);
-
-            new(M_op, Iz, UL90, UR90, γ, τ, dt, α, ω, ν0, bw, dfreq, ρ0, nothing, nothing)
-
-        end
+        return f
 
     end
 
-    mutable struct temp_parameters
-
-        # operators
-        M_op
-        Iz
-        UL90
-        UR90
-
-        # time variables
-        γ
-        τ
-        dt
-        nτ
-
-        # interaction variables
-        α
-        ω
-
-        # spin ensemble variables
-        ν0
-        bw
-        dfreq
-        ν
-        P
-        r # positions of spins
-        ρ_init
+    function make_temp_params(params, vars, I)
     
-        # dissipation parameters
-        Lj # collection of jump operators
+        # copy the parameter file
+        f = copy(params);
 
-        function temp_parameters(params, vars, I)
-
-            # get the names of parameters
-            fields = string.(fieldnames(make_parameters));
-
-            # assign all variable values
-            for x in fields
-                f = getproperty(params, Symbol(x));
-                @eval $(Symbol(x)) = $f;
-            end
-
-            # reassign loop variables based on this iteration
-            for x in intersect(vars, fields)
-                f = getproperty(params, Symbol(x));
-                idx = findall(y -> y == x, vars)[1];
-                f = f[I[idx]];
-                @eval $(Symbol(x)) = $f;
-            end
-
-            # calculate temporary variables
-            nτ = convert(Int64, round(τ*γ/dt));
-            ν = collect((ν0 - bw/2):dfreq:(ν0 + bw/2));
-            P = lorentzian.(ν, ν0, 0.05);
-            P = P/sum(P) # normalize!
-            ρ_init = [params.ρ0 for i = 1:size(ν,1)];
-
-            new(M_op, Iz, UL90, UR90, γ, τ, dt, nτ, α, ω, ν0, bw, dfreq, ν, P, r, ρ_init, Lj)
-
+        # reassign loop variables based on iteration
+        for v in vars
+            idx = findall(y -> y == v, vars)[1];
+            temp = f[v];
+            f[v] = temp[I[idx]];
         end
 
+        ## calculate temporary variables ##
+
+        # number of points in time
+        f["nτ"] = convert(Int64, round(f["τ"]*f["γ"]/f["dt"]));
+
+        # frequency sample & weights
+        f["ν"] = collect(LinRange(f["ν0"] - f["bw"]/2, f["ν0"] + f["bw"]/2, f["nfreq"]));
+        f["P"] = lorentzian.(f["ν"], f["ν0"], 0.05)/sum(lorentzian.(f["ν"], f["ν0"], 0.05));
+
+        # initial positions
+        f["ρ_init"] = [f["ρ0"] for i = 1:f["nfreq"]];
+
+        return f
+
+    end
+
+    function make_lattice(params)
+    
+        # load parameters
+        hlk = params["hlk"];
+        θ = params["θ"];
+        nfreq = params["nfreq"];
+    
+        # calculate the dimension of the lattice
+        dim = size(hlk, 1);
+
+        # zero fill the θ and hlk arrays
+        while size(hlk, 1) < 3
+            push!(hlk, 0);
+        end
+        while size(θ, 1) < 3
+            push!(θ, 0);
+        end
+
+        # create a tuple with the dimensions needed
+        d = ();
+        n = convert(Int64, round(nfreq^(1/dim)));
+        for i = 1:dim
+            d = (d..., n);
+        end
+
+        # fill with ones for the unused dimensions
+        while size(d,1) < 3
+            d = (d..., 1);
+        end
+
+        # fill with unique integers
+        f = sample(1:nfreq, d, replace = false)
+
+        # replace integers with indices
+        I = [findall(x -> x == temp, f)[1] for temp in f]
+
+        # calculate positions
+        r = zeros(nfreq, dim)
+        i = 1;
+        for idx in I
+            x = (idx[1]-1)*hlk[1] + (idx[2]-1)*hlk[2]*cos(θ[1]) + (idx[3]-1)*hlk[3]*cos(θ[2])*sin(θ[3]);
+            y = (idx[2]-1)*hlk[2]*sin(θ[1]) + (idx[3]-1)*hlk[3]*sin(θ[2])*sin(θ[3]);
+            z = (idx[3]-1)*hlk[3]*cos(θ[3]);
+            temp = [x; y; z];
+            r[i,:] = round.(temp[1:dim], digits = 5);
+            i += 1;
+        end
+        idx_r = sample(1:nfreq, nfreq, replace = false);
+    
+        return r, idx_r
+    
     end
 
     function make_idx(vars, params)
 
-        fields = string.(fieldnames(make_parameters));
-
         # create the tuple with the dimensions needed
         d = ();
         p = 1;
-        for x in intersect(vars, fields)
-            f = getproperty(params, Symbol(x));
+        for v in vars
+            f = params[v];
             lf = size(f,1)
             p = p*lf;
             d = (d..., lf);
@@ -142,7 +146,7 @@ module SpinSimParams
         return G
     end
 
-    export make_parameters, temp_parameters, make_idx
+    export make_params, make_temp_params, make_idx, make_lattice
     export lorentzian, gaussian
 
 end
--- spin_sims.jl
+++ spin_sims.jl
@@ -1,25 +1,27 @@
 include("liouville_tools.jl")
 using .LiouvilleTools
+using LinearAlgebra
 
-function spin_echo_sim(params)   
+function spin_echo_sim(params)
+    
     # initialize M_list
     M_list = [];
     
-    UL90 = params.UL90
-    UR90 = params.UR90
+    UL90 = params["UL90"];
+    UR90 = params["UR90"];
     
     # 90 pulse
-    ρ_list = [UL90*ρ*UR90 for ρ in params.ρ_init];
+    ρ_list = [UL90*ρ*UR90 for ρ in params["ρ_init"]];
     
     # first tau
     t0 = 0.0;
-    ρ_list, M_list, t1 = time_propagate(ρ_list, M_list, t0, params.dt, params.nτ, params)
+    ρ_list, M_list, t1 = time_propagate(ρ_list, M_list, t0, params["dt"], params["nτ"], params)
     
     # 180 pulse
     ρ_list = [UL90*UL90*ρ*UR90*UR90 for ρ in ρ_list];
         
     # second tau
-    ρ_list, M_list, t2 = time_propagate(ρ_list, M_list, t1, params.dt, 2*params.nτ, params)
+    ρ_list, M_list, t2 = time_propagate(ρ_list, M_list, t1, params["dt"], 2*params["nτ"], params)
     
   return M_list
 
@@ -28,18 +30,20 @@ end
 function time_propagate(ρ_list, M_list, t0, dt, nsteps, params)
         
     # spectrum info
-    ν0 = params.ν0 # central freq.
-    ν = params.ν # spin freqs.
-    P = params.P # spin weights
-    nS = size(P,1) # number of spins
+    ν0 = params["ν0"] # central freq.
+    ν = params["ν"] # spin freqs.
+    P = params["P"] # spin weights
+    nS = params["nfreq"] # number of spins
+    r = params["r"] # positions of spins
     
     # operators
-    M_op = params.M_op
-    Iz = params.Iz
+    M_op = params["M_op"]
+    Iz = params["Iz"]
     
     # interaction parameters
-    α = params.α
-    ω = params.ω
+    α = params["α"]
+    ω = params["ω"]
+    k = params["k"]
     
     # initial time
     t = t0;
@@ -52,11 +56,10 @@ function time_propagate(ρ_list, M_list, t0, dt, nsteps, params)
         
         t += dt;
         
-        
         Oprime = getOprime(t, M, params)
 
-        UL = [exp(-1im*( -(ν[j]-ν0)*Iz - α*cos(ω*t)*Oprime )*dt) for j = 1:nS];
-        UR = [exp( 1im*( -(ν[j]-ν0)*Iz - α*cos(ω*t)*Oprime )*dt) for j = 1:nS];        
+        UL = [exp(-1im*( -(ν[j]-ν0)*Iz - α*cos(ω*t + dot(k, r[j,:]))*Oprime )*dt) for j = 1:nS];
+        UR = [exp( 1im*( -(ν[j]-ν0)*Iz - α*cos(ω*t + dot(k, r[j,:]))*Oprime )*dt) for j = 1:nS];        
         
         # time evolve
         ρ_list = [UL[j]*ρ_list[j]*UR[j] for j = 1:nS];
@@ -73,7 +76,7 @@ end
 
 function getOprime(t, M, params)
    
-    ν0 = params.ν0
+    ν0 = params["ν0"]
     # Oprime = (1im/2)*[0 -exp(-1im*ν0*t); exp(1im*ν0*t) 0]; # Iy
     Oprime = (1/4)*[0 conj(M); M 0] - (1/4)*[0 M*exp(-2im*ν0*t); conj(M)*exp(2im*ν0*t) 0]; # IyMy
     # Oprime = (1im/4)*[0 -conj(M); M 0] + (1im/4)*[0 -M*exp(-2im*ν0*t); conj(M)*exp(2im*ν0*t) 0]; # IyMx
@@ -85,24 +88,25 @@ function getOprime(t, M, params)
 end
 
 function spin_echo_sim_liouville(params)   
+    
     # initialize M_list
     M_list = [];
     
-    UL90 = params.UL90
-    UR90 = params.UR90    
+    UL90 = params["UL90"]
+    UR90 = params["UR90"]    
     
     # 90 pulse
-    ρ_list_L = [dm_H2L(UL90*ρ*UR90) for ρ in params.ρ_init];    
+    ρ_list_L = [dm_H2L(UL90*ρ*UR90) for ρ in params["ρ_init"]];    
     
     # first tau
     t0 = 0.0;
-    ρ_list_L, M_list, t1 = time_propagate_liouville(ρ_list_L, M_list, t0, params.dt, params.nτ, params)
+    ρ_list_L, M_list, t1 = time_propagate_liouville(ρ_list_L, M_list, t0, params["dt"], params["nτ"], params)
     
     # 180 pulse
     ρ_list_L = [dm_H2L(UL90*UL90* dm_L2H(ρ_L) *UR90*UR90) for ρ_L in ρ_list_L];
         
     # second tau
-    ρ_list_L, M_list, t2 = time_propagate_liouville(ρ_list_L, M_list, t1, params.dt, 2*params.nτ, params)
+    ρ_list_L, M_list, t2 = time_propagate_liouville(ρ_list_L, M_list, t1, params["dt"], 2*params["nτ"], params)
     
   return M_list
 
@@ -111,24 +115,24 @@ end
 function time_propagate_liouville(ρ_list_L, M_list, t0, dt, nsteps, params) 
             
     # spectrum info
-    ν0 = params.ν0 # central freq.
-    ν = params.ν # spin freqs.
-    P = params.P # spin weights
-    nS = size(P,1) # number of spins
+    ν0 = params["ν0"] # central freq.
+    ν = params["ν"] # spin freqs.
+    P = params["P"] # spin weights
+    nS = params["nfreq"] # number of spins
     
     # operators
-    M_op = params.M_op
-    Iz = params.Iz
+    M_op = params["M_op"]
+    Iz = params["Iz"]
     
     # interaction parameters
-    α = params.α
-    ω = params.ω
+    α = params["α"]
+    ω = params["ω"]
     
     # initial time
     t = t0;
 
     # jump operators
-    Lj_list = params.Lj
+    Lj_list = params["Lj"]
     J_L = JumpsToSuper(Lj_list) # get dissipative super operator (assumed constant in time)
   
     # initial magnetization    
@@ -139,10 +143,9 @@ function time_propagate_liouville(ρ_list_L, M_list, t0, dt, nsteps, params)
         
         t = t + dt;
         
-        
         Oprime = getOprime(t, M, params)
         
-        Ham_L = [HamToSuper( -(ν[j]-ν0)*Iz - α*cos(ω*t)*Oprime ) for j = 1:nS]
+        Ham_L = [HamToSuper( -(ν[j]-ν0)*Iz - α*cos(ω*t + dot(k, r[j,:]))*Oprime ) for j = 1:nS]
 
         U_L = [exp((-1im*Ham_L[j] + J_L )*dt) for j = 1:nS]      
         
